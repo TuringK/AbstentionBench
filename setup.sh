bcb3dbac40cdf340ba66583d2d48dec618fadf34
Artem Vakhutinskiy <vahut@yandex.ru>
2025-11-07 22:59:57 +0000

    all models can be submitted to slurm with judges for all datasets

--- setup.sh
+++ setup.sh
@@ -1,25 +1,28 @@
 # Create and activate the new environment as per the environment.yml file
-# mamba env create -f environment.yml
+mamba env create -f environment.yml
 
-# mamba activate abstention-bench
+mamba activate abstention-bench
 
-# Install VLLM and PyTorch using pip, because we need specific CUDA-compatible versions
-# pip install tiktoken sentencepiece --only-binary :all:
-pip install xformers==0.0.29.post2
-pip install vllm==0.8.5.post1 --extra-index-url https://download.pytorch.org/whl/cu124
- 
+# Install uv for faster and clearer dependacy management
+pip install uv
+
+# We need PyTorch fist to build vllm dependancies
 if [[ $OSTYPE == "darwin"* ]]; then
-  pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 -U 
+  uv pip install torch==2.6.0 
 else
-  pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 -U --index-url https://download.pytorch.org/whl/cu124
+  uv pip install torch==2.6.0 --index-url https://download.pytorch.org/whl/cu124
 fi 
 
-pip install -e .
+uv pip install tiktoken sentencepiece --only-binary :all:
+uv pip install xformers==0.0.29.post2 --no-build-isolation # build will use already installed Pytorch
+uv pip install vllm==0.8.5.post1 --torch-backend=cu124 # --extra-index-url https://download.pytorch.org/whl/cu124
+
+uv pip install -e .
 
 # Test that PyTorch is installed correctly
-# pytorch_version_output=`python -c "import torch; print(torch.__version__)"`
-# if [[ $pytorch_version_output == *"121"* ]]; then
-#   echo "PyTorch is installed with cuda 12.1!"
-# else
-#   echo "PyTorch installation missing cuda 12.1. Please install with pip using official instructions"
-# fi
+pytorch_version_output=`python -c "import torch; print(torch.__version__)"`
+if [[ $pytorch_version_output == *"124"* ]]; then
+  echo "PyTorch is installed with cuda 12.4!"
+else
+  echo "PyTorch installation missing cuda 12.4. Please install with pip using official instructions"
+fi
