b04df4d64b62f60a49b6a6a58937db7464dc2089
Artem Vakhutinskiy <vahut@yandex.ru>
2025-12-05 14:18:12 +0000

    [fix] vectors sbatch script

--- scripts/vectors_extraction.sh
+++ scripts/vectors_extraction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=caa_extract
-#SBATCH --output=scripts/logs/extract/extract_%A_%a.out
-#SBATCH --error=scripts/logs/extract/extract_%A_%a.err
+#SBATCH --output=scripts/logs/extract/%j/extract_%A_%a.out
+#SBATCH --error=scripts/logs/extract/%j/extract_%A_%a.err
 #SBATCH --time=02:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=82G
@@ -14,13 +14,10 @@ source ./activate.sh
 PYTHON_BIN="/mnt/parscratch/users/${USER_NAME}/private/mamba/envs/abstention-bench/bin/python"
 DATA_PATH="/mnt/parscratch/users/${USER_NAME}/private/projects/AbstentionBench/data/sample_pairs.csv"
 
-OUTPUT_DIR_NAME="qwen_2_5_1_5B_"${SLURM_JOB_ID}
+OUTPUT_DIR_NAME="Qwen/Qwen2_5_1_5B_Instruct/${SLURM_JOB_ID}"
 OUTPUT_PATH="/mnt/parscratch/users/${USER_NAME}/private/projects/AbstentionBench/data/${OUTPUT_DIR_NAME}"
 
 MODEL_NAME="Qwen/Qwen2.5-1.5B-Instruct"
-
-# Create output directories
-mkdir -p ${OUTPUT_PATH}
 
 # $SLURM_ARRAY_TASK_ID serves as the layer_idx
 export PYTHONPATH=$PYTHONPATH:$(pwd)
